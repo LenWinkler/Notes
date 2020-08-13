414710e16435970824b492918758af2854bdc368
LenWinkler <lenwinkler86@gmail.com>
2020-08-13 14:53:40 -0400

    add DbContext and Note migration

---
+++ Notes/Models/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace Notes.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Note> Notes { get; set; }
+    }
+}
