e861651eefaadffdfcba433a1a1fe3e2786afed9
LenWinkler <lenwinkler86@gmail.com>
2020-08-13 00:31:40 -0400

    created Note model

---
+++ Notes/Models/Note.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Notes.Models
+{
+    public class Note
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Title { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(8000)")]
+        public string Text { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:d}")]
+        public DateTime Date_Created { get; set; }
+    }
+}
